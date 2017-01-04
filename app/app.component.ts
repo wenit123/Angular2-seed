1ba6c2e3fea49fa0f346d54c66d08497396d42af
Iting Wen <wenit123@gmail.com>
2017-01-04 15:18:30 -0800

    add gitignore & using componrnt to the root

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,9 @@
 import {Component} from 'angular2/core';
+import {CoursesComponent} from './courses.component'
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: '<h1>My First Angular 2 App</h1><courses></courses>',
+    directives: [CoursesComponent] // ref
 })
-export class AppComponent { }
+export class AppComponent { } // this is the root application
