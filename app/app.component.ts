63dfe773de808743404c4d933e9062c74b3673d9
Iting Wen <wenit123@gmail.com>
2017-01-04 16:30:30 -0800

    try your own for Author-component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,9 +1,10 @@
 import {Component} from 'angular2/core';
 import {CoursesComponent} from './courses.component'
+import {AuthorsComponent} from './authors.component'
 
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1><courses></courses>',
-    directives: [CoursesComponent] // ref
+    template: '<h1>My First Angular 2 App</h1><courses></courses><authors><authors>',
+    directives: [CoursesComponent, AuthorsComponent] // ref
 })
 export class AppComponent { } // this is the root application
